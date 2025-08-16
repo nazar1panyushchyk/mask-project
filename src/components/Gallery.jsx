6902a77d53c16835296b6d8244d9dbac07939c2c
nazar1panyushchyk <npanusik@gmail.com>
2025-08-17 00:19:27 +0300

    completed gallery's functional

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,93 +4,159 @@ import galleryData from "../galleryData.json";
 import "../css/gallery.css";
 
 export default class Gallery extends React.Component {
-    render() {
-        const categories = [...new Set(galleryData.map((item) => item.category))];
-        return (
-          <div className="gallery">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+  state = {
+    selectedCategory: null,
+  };
+
+  handleCategory = (categoryName) => {
+    this.setState({ selectedCategory: categoryName });
+  };
+
+  render() { 
+    const { selectedCategory } = this.state;
+    const categories = [...new Set(galleryData.map((item) => item.category))];
+    return (
+      <div className="gallery">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Обрати категорію
+          </button>
+          <ul className="dropdown-menu">
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory("Овочі, фрукти, гриби")}
               >
-                Обрати категорію
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Овочі, фрукти, гриби
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Звірята
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Новий рік
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Квіти, природні явища
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Свято осені
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Хелловін
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Казкові, мультгерої
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Ляльки, цукерки
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Пірати, моряки, солдати
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Принци, королі, мушкетери
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Ангели
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Народні костюми
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Сукні
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Східні, китайські костюми
-                  </a>
-                </li>
-              </ul>
-            </div>
-            {categories.map((category) => (
+                Овочі, фрукти, гриби
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory("Звірята")}
+              >
+                Звірята
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Новий рік
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Квіти, природні явища
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Свято осені
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Хелловін
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Казкові, мультгерої
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Ляльки, цукерки
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Пірати, моряки, солдати
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Принци, королі, мушкетери
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Ангели
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Народні костюми
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Сукні
+              </a>
+            </li>
+            <li>
+              <a
+                className="dropdown-item"
+                href="#"
+                onClick={() => this.handleCategory(categoryName)}
+              >
+                Східні, китайські костюми
+              </a>
+            </li>
+          </ul>
+        </div>
+        {selectedCategory === null
+          ? categories.map((category) => (
               <div key={category}>
                 <h2>{category}</h2>
 
@@ -102,8 +168,24 @@ export default class Gallery extends React.Component {
                     ))}
                 </div>
               </div>
-            ))}
-          </div>
-        );
-    }
-};
+            ))
+          : ( 
+             <div>
+                <h2>{selectedCategory}</h2>
+                <div className="gallery-image">
+                  {galleryData
+                    .filter((item) => item.category === selectedCategory)
+                    .map(({ image }, idx) => (
+                      <img
+                        key={idx}
+                        src={images[image]}
+                        alt={selectedCategory}
+                      />
+                    ))}
+                </div>
+              </div>
+            )}
+      </div>
+    );
+  }
+}
